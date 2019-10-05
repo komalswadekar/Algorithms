4d6dab3cae88a39960e84e2df37e995bf8e187c1
Komal Wadekar <55719939+komalswadekar@users.noreply.github.com>
2019-10-05 18:03:02 -0400

    Linear Search to search given element in an array

---
+++ search/LinearSearch.java
@@ -0,0 +1,25 @@
+/*
+ * Linear search to search element x in given array[] until the element is found
+ * If x is present returns its position in array else returns -1
+ * Works with sorted and unsorted arrays
+ * author Komal Soham Wadekar
+ */
+package search;
+
+public class LinearSearch {
+
+	public int search(int arr[],int searchItem){
+		for(int i=0; i<arr.length; i++){
+			if(arr[i]==searchItem)
+				return i;			
+		}	
+		return -1;
+	}
+	
+	public static void main(String args[]){
+		int arr[] = {2,7,19,8,25,8,19,0,5};
+		int searchItem = 8;		
+		LinearSearch linear = new LinearSearch();
+		System.out.print("Element found at position: " +linear.search(arr,searchItem));
+	}
+}
